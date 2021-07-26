4487667e293b2ba9bb05a8b3992341df7d269b7d
Sugarbell <anonesayu@gmail.com>
2021-07-25 19:13:32 -0600

    # Improvement....

---
+++ SayuBot/helper/mongo_connect.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from .mongo_db import *
+
+# Variables
+load_dotenv()
+URI = os.getenv("URI")
+
+
+async def confirm(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.find(data)
+
+
+async def add_(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.insert_one(data)
+
+
+async def update_(user_db, old_data=None, new_data=None):
+    if old_data is None:
+        old_data = {}
+    if new_data is None:
+        new_data = {}
+    return user_db.update_one(old_data, new_data)
+
+
+async def remove_(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.delete_one(data)
+
+
