8c8beea35af3229d24cdbdd7b03063e9c52a4346
Sugarbell <anonesayu@gmail.com>
2021-07-26 00:47:48 -0600

    # Improvement....

--- SayuBot/helper/mongo_connect.py
+++ SayuBot/helper/mongo_connect.py
@@ -33,3 +33,33 @@ async def remove_(user_db, data=None):
     return user_db.delete_one(data)
 
 
+def confirm_ofdb(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.find(data)
+
+
+def add_ofdb(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.insert_one(data)
+
+
+def update_ofdb(user_db, old_data=None, new_data=None):
+    if old_data is None:
+        old_data = {}
+    if new_data is None:
+        new_data = {}
+    return user_db.update_one(old_data, new_data)
+
+
+def remove_ofdb(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.delete_one(data)
+
+
+def remove_many(user_db, data=None):
+    if data is None:
+        data = {}
+    return user_db.delete_many(data)
