9404211eecd8a6e66393c68b07034e12d4bb765b
Sugarbell <anonesayu@gmail.com>
2021-07-25 20:17:04 -0600

    # Improvement....

--- SayuBot/plugins/restart.py
+++ SayuBot/plugins/restart.py
@@ -1,11 +1,33 @@
-from SayuBot import SayuBot
+import os
+import heroku3
+from dotenv import load_dotenv
 from telethon import events, Button
+from SayuBot import SayuBot, OWNER_ID
+
+# ADMINS and COMMANDS
+load_dotenv()
+APP_NAME = os.getenv("APP_NAME")
+# ADMINS_STR = os.getenv("ADMINS")
+HEROKU_API = os.getenv("HEROKU_API")
+CLIENT = heroku3.from_key(HEROKU_API)
+# ADMINS = [int(i) for i in ADMINS_STR.split(" ")]
 
 
 @SayuBot.on(events.NewMessage(pattern=r"\.rt"))
 async def restart(event):
-    await SayuBot.disconnect()
+    print(event)
+    user = event.sender_id
+    if user == OWNER_ID:
+        await event.edit(f"Reiniciando {APP_NAME}...")
+        app = CLIENT.app(APP_NAME)
+        app.restart()
 
 
+@SayuBot.on(events.NewMessage(pattern=r"\.rc"))
+async def restart(event):
+    print(event)
+    user = event.sender_id
+    if user == OWNER_ID:
+        await SayuBot.disconnect()
 
 
